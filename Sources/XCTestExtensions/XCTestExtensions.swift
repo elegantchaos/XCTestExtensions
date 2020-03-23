7cf4c6f731e91a6bd1d2c22ddb0f1dd63674c883
Sam Deane <sam@elegantchaos.com>
2020-03-23 17:44:39 +0000

    Added better assertion for comparing strings

--- Sources/XCTestExtensions/XCTestExtensions.swift
+++ Sources/XCTestExtensions/XCTestExtensions.swift
@@ -173,3 +173,40 @@ public func XCTAssertSuccess<S,F>(_ result: Result<S,F>, message: String = "Fail
         }
     }
 }
+
+/// Assert that two strings are equal.
+/// If the strings don't match, this function compares them line by line to produce a more accurate description of the place where they differ.
+/// It optionally ignores whitespace at the beginning/end of each line.
+public func XCTAssertEqual(_ string: String, _ expected: String, ignoringWhitespace: Bool = false, file: StaticString = #file, line fileLine: UInt = #line) {
+    if string != expected {
+        let lines = string.split(separator: "\n")
+        let expectedLines = expected.split(separator: "\n")
+        let lineCount = lines.count
+        let expectedLineCount = expectedLines.count
+        for n in 0 ..< min(lineCount, expectedLineCount) {
+            var line = String(lines[n])
+            var expectedLine = String(expectedLines[n])
+            if ignoringWhitespace {
+                line = line.trimmingCharacters(in: .whitespaces)
+                expectedLine = expectedLine.trimmingCharacters(in: .whitespaces)
+            }
+            
+            if line != expectedLine {
+                XCTFail("Strings different at line \(n).\n\n\"\(line)\"\n\nvs.\n\n\"\(expectedLine)\"", file: file, line: fileLine)
+                return
+            }
+        }
+        
+        if lineCount < expectedLineCount {
+            XCTFail("\(expectedLineCount - lineCount) lines missing from string.", file: file, line: fileLine)
+        } else if lineCount > expectedLineCount {
+            XCTFail("\(lineCount - expectedLineCount) extra lines in string.", file: file, line: fileLine)
+        }
+    }
+}
+
+/// Assert that two strings are equal, ignoring whitespace at the beginning/end of each line.
+/// If the strings don't match, this function compares them line by line to produce a more accurate description of the place where they differ.
+public func XCTAssertEqualIgnoringWhitespace(_ string: String, _ expected: String, file: StaticString = #file, line: UInt = #line) {
+    XCTAssertEqual(string, expected, ignoringWhitespace: true, file: file, line: line)
+}
