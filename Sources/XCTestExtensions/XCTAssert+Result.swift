be38a3150f381e9cae37193fed620bc8ee6103c3
Sam Deane <sam@elegantchaos.com>
2020-05-31 15:45:57 +0100

    split up code a bit

---
+++ Sources/XCTestExtensions/XCTAssert+Result.swift
@@ -0,0 +1,55 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//  Created by Sam Deane on 31/05/2020.
+//  All code (c) 2020 - present day, Elegant Chaos Limited.
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+#if !os(watchOS)
+import XCTest
+
+/// Assert that a result is a success.
+/// - Parameter result: The result to check.
+/// - Parameter message: A message to print if the assertion fails.
+/// - Parameter expectation: An expectation to fullfill if the assertion fails, or if it succeeds with no continuation provided.
+/// - Parameter continuation: A continuation method to call if the assertion succeeded.
+///
+/// In an asynchronous setting, you're typically waiting for an expectation to be fullfilled before ending the test.
+/// If this assertion failed, you may want to exit the tests early, which is why we accept an expectation to fullfill.
+/// Conversely if the assertion succeeds, you may want to perform extra work, which is why we accept a continuation block.
+/// If no continuation is provided, but an expectation was provided, we call the expectation if the assertion succeeds, on the
+/// assumption that there's nothing more to do and we want to signal that the test is done.
+
+public func XCTAssertSuccess<S,F>(_ result: Result<S,F>, message: String = "Failure", expectation: XCTestExpectation? = nil, continuation: ((S) -> Void)? = nil) {
+    switch result {
+    case .failure(let error):
+        XCTFail("\(message): \(error)")
+        expectation?.fulfill()
+    case .success(let success):
+        if let continuation = continuation {
+            continuation(success)
+        } else {
+            expectation?.fulfill()
+        }
+    }
+}
+
+#if os(macOS) || os(Linux)
+/// Assert that the result of running an external executable matches expected values.
+public func XCTAssertResult(_ result: XCTestRunner.Result, status: Int32, stdout: String, stderr: String, ignoringWhitespace: Bool = true, file: StaticString = #file, line: UInt = #line) {
+    switch result {
+        case .ok(let out, let err):
+            XCTAssertEqual(status, 0)
+            XCTAssertEqualLineByLine(out, stdout, ignoringWhitespace: ignoringWhitespace, file: file, line: line)
+            XCTAssertEqualLineByLine(err, stderr, ignoringWhitespace: ignoringWhitespace, file: file, line: line)
+        
+    case .failed(let code, let out, let err):
+            XCTAssertEqual(code, status)
+            XCTAssertEqualLineByLine(out, stdout, ignoringWhitespace: ignoringWhitespace, file: file, line: line)
+            XCTAssertEqualLineByLine(err, stderr, ignoringWhitespace: ignoringWhitespace, file: file, line: line)
+
+    case .launchError(let error):
+        XCTFail("launch error \(error)", file: file, line: line)
+    }
+}
+#endif
+
+#endif
---
+++ Sources/XCTestExtensions/XCTAssertEqual+String.swift
@@ -0,0 +1,55 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//  Created by Sam Deane on 31/05/2020.
+//  All code (c) 2020 - present day, Elegant Chaos Limited.
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+#if !os(watchOS)
+import XCTest
+import Foundation
+
+/// Assert that two strings are equal.
+/// If the strings don't match, this function compares them line by line to produce a more accurate description of the place where they differ.
+/// It optionally ignores whitespace at the beginning/end of each line.
+public func XCTAssertEqualLineByLine(_ string: String, _ expected: String, ignoringWhitespace: Bool = false, file: StaticString = #file, line fileLine: UInt = #line) {
+    if string != expected {
+        let lines = string.split(separator: "\n")
+        let expectedLines = expected.split(separator: "\n")
+        let lineCount = lines.count
+        let expectedLineCount = expectedLines.count
+        for n in 0 ..< min(lineCount, expectedLineCount) {
+            var line = String(lines[n])
+            var expectedLine = String(expectedLines[n])
+            if ignoringWhitespace {
+                line = line.trimmingCharacters(in: .whitespaces)
+                expectedLine = expectedLine.trimmingCharacters(in: .whitespaces)
+            }
+            
+            if line != expectedLine {
+                XCTFail("Strings different at line \(n).\n\n\"\(line)\"\n\nvs.\n\n\"\(expectedLine)\"", file: file, line: fileLine)
+                return
+            }
+        }
+        
+        if lineCount < expectedLineCount {
+            let missing = expectedLines[lineCount ..< expectedLineCount].joined(separator: "\n")
+            XCTFail("\(expectedLineCount - lineCount) lines missing from string.\n\n\(missing)", file: file, line: fileLine)
+        } else if lineCount > expectedLineCount {
+            let extra = lines[expectedLineCount ..< lineCount].joined(separator: "\n")
+            XCTFail("\(lineCount - expectedLineCount) extra lines in string.\n\n\(extra)", file: file, line: fileLine)
+        }
+    }
+}
+
+/// Assert that two strings are equal, ignoring whitespace at the beginning/end of each line.
+/// If the strings don't match, this function compares them line by line to produce a more accurate description of the place where they differ.
+public func XCTAssertEqual(_ string: String, _ expected: String, file: StaticString = #file, line: UInt = #line) {
+    XCTAssertEqualLineByLine(string, expected, ignoringWhitespace: false, file: file, line: line)
+}
+
+/// Assert that two strings are equal, ignoring whitespace at the beginning/end of each line.
+/// If the strings don't match, this function compares them line by line to produce a more accurate description of the place where they differ.
+public func XCTAssertEqualIgnoringWhitespace(_ string: String, _ expected: String, file: StaticString = #file, line: UInt = #line) {
+    XCTAssertEqualLineByLine(string, expected, ignoringWhitespace: true, file: file, line: line)
+}
+
+#endif
