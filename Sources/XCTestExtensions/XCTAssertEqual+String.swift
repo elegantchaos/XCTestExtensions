3790249165d3070e744339ef8bb8a86abb1dafa2
Sam Deane <sam@elegantchaos.com>
2021-04-29 11:37:46 +0100

    added XCTCheck

--- Sources/XCTestExtensions/XCTAssertEqual+String.swift
+++ Sources/XCTestExtensions/XCTAssertEqual+String.swift
@@ -10,32 +10,13 @@ import Foundation
 /// If the strings don't match, this function compares them line by line to produce a more accurate description of the place where they differ.
 /// It optionally ignores whitespace at the beginning/end of each line.
 public func XCTAssertEqualLineByLine(_ string: String, _ expected: String, ignoringWhitespace: Bool = false, file: StaticString = #file, line fileLine: UInt = #line) {
-    if string != expected {
-        let lines = string.split(separator: "\n")
-        let expectedLines = expected.split(separator: "\n")
-        let lineCount = lines.count
-        let expectedLineCount = expectedLines.count
-        for n in 0 ..< min(lineCount, expectedLineCount) {
-            var line = String(lines[n])
-            var expectedLine = String(expectedLines[n])
-            if ignoringWhitespace {
-                line = line.trimmingCharacters(in: .whitespaces)
-                expectedLine = expectedLine.trimmingCharacters(in: .whitespaces)
-            }
-            
-            if line != expectedLine {
-                XCTFail("strings different at line \(n).\n\nwas:\n\n\"\(line)\"\n\nexpected:\n\n\"\(expectedLine)\"\n", file: file, line: fileLine)
-                return
-            }
-        }
-        
-        if lineCount < expectedLineCount {
-            let missing = expectedLines[lineCount ..< expectedLineCount].joined(separator: "\n")
-            XCTFail("\(expectedLineCount - lineCount) lines missing from string.\n\n\(missing)", file: file, line: fileLine)
-        } else if lineCount > expectedLineCount {
-            let extra = lines[expectedLineCount ..< lineCount].joined(separator: "\n")
-            XCTFail("\(lineCount - expectedLineCount) extra lines in string.\n\n\(extra)", file: file, line: fileLine)
-        }
+    do {
+        let options: XCTCheckOptions = ignoringWhitespace ? [.ignoringWhitespace] : []
+        try XCTCheck(string, matches: expected, options: options, file: file, line: fileLine)
+    } catch XCTCheckFailure.strings(let details) {
+        XCTFail(details.description)
+    } catch {
+        XCTFail("Unexpected error thrown")
     }
 }
 
---
+++ Sources/XCTestExtensions/XCTCheck.swift
@@ -0,0 +1,109 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//  Created by Sam Deane on 29/04/21.
+//  All code (c) 2021 - present day, Elegant Chaos Limited.
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+import Foundation
+
+public struct XCTCheckOptions: OptionSet {
+    public let rawValue: Int
+    public init(rawValue: Int) {
+        self.rawValue = rawValue
+    }
+
+    static let ignoringWhitespace = Self(rawValue: 1 << 0)
+    
+    public static let `default`: XCTCheckOptions = []
+}
+
+public enum XCTCheckFailure: Error {
+    public struct StringDetails {
+        let description: String
+        let value: String
+        let expected: String
+        let file: StaticString
+        let line: UInt
+
+        internal init(_ description: String, value: String, expected: String, file: StaticString, line: UInt) {
+            self.description = description
+            self.value = value
+            self.expected = expected
+            self.file = file
+            self.line = line
+        }
+    }
+    
+    public struct IntDetails {
+        let description: String
+        let value: Int
+        let expected: Int
+        let file: StaticString
+        let line: UInt
+    }
+    
+    static func failure(_ message: String, value string: String, matches expected: String, ignoringWhitespace: Bool = false, file: StaticString = #file, line fileLine: UInt = #line) -> Self {
+        return .strings(StringDetails(message, value: string, expected: expected, file: file, line: fileLine))
+    }
+
+    case strings(StringDetails)
+    case integers(IntDetails)
+}
+
+public protocol Checkable {
+    func matches(_ other: Self, options: XCTCheckOptions, file: StaticString, line: UInt) throws
+}
+
+public func XCTCheck<T>(_ value: T, matches other: T, options: XCTCheckOptions = .default, file: StaticString = #file, line: UInt = #line) throws where T: Checkable {
+    try value.matches(other, options: options, file: file, line: line)
+}
+
+public func XCTCheck<T, I>(_ key: KeyPath<T, I>, of value: T, matches other: T, options: XCTCheckOptions = .default, file: StaticString = #file, line: UInt = #line) throws where I: Checkable {
+    try value[keyPath: key].matches(other[keyPath: key], options: options, file: file, line: line)
+}
+
+/// Check that two strings are equal.
+/// If the strings don't match, this function compares them line by line to produce a more accurate description of the place where they differ.
+/// It optionally ignores whitespace at the beginning/end of each line.
+extension String: Checkable {
+    public func matches(_ expected: String, options: XCTCheckOptions, file: StaticString = #file, line: UInt = #line) throws {
+        func failure(_ message: String) -> XCTCheckFailure {
+            return XCTCheckFailure.failure(message, value: self, matches: expected, file: file, line: line)
+        }
+    
+        let ignoringWhitespace = options.contains(.ignoringWhitespace)
+        if self != expected {
+            let lines = self.split(separator: "\n")
+            let expectedLines = expected.split(separator: "\n")
+            let lineCount = lines.count
+            let expectedLineCount = expectedLines.count
+            for n in 0 ..< min(lineCount, expectedLineCount) {
+                var line = String(lines[n])
+                var expectedLine = String(expectedLines[n])
+                if ignoringWhitespace {
+                    line = line.trimmingCharacters(in: .whitespaces)
+                    expectedLine = expectedLine.trimmingCharacters(in: .whitespaces)
+                }
+                
+                if line != expectedLine {
+                    throw failure("strings different at line \(n).\n\nwas:\n\n\"\(line)\"\n\nexpected:\n\n\"\(expectedLine)\"\n")
+                }
+            }
+            
+            if lineCount < expectedLineCount {
+                let missing = expectedLines[lineCount ..< expectedLineCount].joined(separator: "\n")
+                throw failure("\(expectedLineCount - lineCount) lines missing from string.\n\n\(missing)")
+            } else if lineCount > expectedLineCount {
+                let extra = lines[expectedLineCount ..< lineCount].joined(separator: "\n")
+                throw failure("\(lineCount - expectedLineCount) extra lines in string.\n\n\(extra)")
+            }
+        }
+    }
+}
+
+extension Int: Checkable {
+    public func matches(_ other: Int, options: XCTCheckOptions, file: StaticString, line: UInt) throws {
+        if self != other {
+            throw XCTCheckFailure.integers(XCTCheckFailure.IntDetails(description: "\(self) != \(other)", value: self, expected: other, file: file, line: line))
+        }
+    }
+}
